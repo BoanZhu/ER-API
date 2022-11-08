8646d530eafdfada883d1ca4b073aec66d716010
Jie <15253292176@163.com>
2022-11-08 12:01:30 +0000

    Test render

--- src/main/java/com/ic/er/Render.java
+++ src/main/java/com/ic/er/Render.java
@@ -1,9 +1,39 @@
 package com.ic.er;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.xml.sax.SAXException;
+
 
 public class Render {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // load page using HTML Unit and fire scripts
+        WebClient webClient = new WebClient();
+        HtmlPage myPage = webClient.getPage(new File("src/main/resources/Render.html").toURI().toURL());
+
+        // convert page to generated HTML and convert to document
+        Document doc = Jsoup.parse(myPage.asXml());
+
+        System.out.println(doc.select("#image"));
+        webClient.close();
+    }
 
     }
-}
+
+
+
