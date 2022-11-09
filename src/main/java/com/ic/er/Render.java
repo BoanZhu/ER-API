265b7b9dc99c5f44a3af82c3f7e4338763a5cd34
Jie <15253292176@163.com>
2022-11-09 12:19:17 +0000

    decode the data

--- src/main/java/com/ic/er/Render.java
+++ src/main/java/com/ic/er/Render.java
@@ -1,60 +1,50 @@
 package com.ic.er;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Base64;
-
-
+import java.util.regex.Pattern;
 
 public class Render {
-    public static void main(String[] args) throws
-            IOException, InterruptedException {
+
+    private final static Pattern pattern = Pattern.compile("^(data:(.*?);base64,)");
+    private final static String FILENAME= "test.png";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // load page using HTML Unit and fire scripts
-        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        WebClient webClient = new WebClient();
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setDownloadImages(true);
         HtmlPage myPage = webClient.getPage(new File("src/main/resources/Render.html").toURI().toURL());
-
-        String imageDate = myPage.getElementById("image").asNormalizedText();
-
-        System.out.println("image: " + myPage.getElementById("image").asNormalizedText());
+        // get the base64 code
+        String baseImageCode = myPage.getElementById("image").asNormalizedText();
+        // close connection
         webClient.close();
-//        decoder(imageDate,"test.png");
-//        try (Playwright playwright = Playwright.create()) {
-//            List<BrowserType> browserTypes = Arrays.asList(
-//                    playwright.chromium()
-//            );
-//            for (BrowserType browserType : browserTypes) {
-//                try (Browser browser = browserType.launch()) {
-//                    BrowserContext context = browser.newContext();
-//                    Page page = context.newPage();
-//                    String filePath = new File("src/main/resources/Render.html").toURI().toURL().toString();
-//                    page.navigate(filePath);
-//                    System.out.println("image: " + page.locator("#image").textContent());
-//                    decoder(page.locator("#image").textContent(),"test2.jpeg");
-//                }
-//            }
-//        }
+
+        //decode the code into image
+        decode(baseImageCode,FILENAME);
+
     }
 
-//    public static void decoder(String base64Image, String pathFile) {
-//        try (FileOutputStream imageOutFile = new FileOutputStream(pathFile)) {
-//            // Converting a Base64 String into Image byte array
-//            byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
-//            imageOutFile.write(imageByteArray);
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Image not found" + e);
-//        } catch (IOException ioe) {
-//            System.out.println("Exception while reading the Image " + ioe);
-//        }
-//    }
+    private static void decode(String baseImageCode, String filename) {
+
+        String base64Data = baseImageCode.split(",")[1];
+
+        try (FileOutputStream imageOutFile = new FileOutputStream(FILENAME)) {
+            byte[] imageByteArray = Base64.getDecoder().decode(base64Data);
+            imageOutFile.write(imageByteArray);
+        } catch (FileNotFoundException e) {
+            System.out.println("Image not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading the Image " + ioe);
+        }
+    }
 }
 
 
