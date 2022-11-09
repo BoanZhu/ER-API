d1dff247f93569f59bf903a836c20194d98802d3
Jie <15253292176@163.com>
2022-11-09 11:07:58 +0000

    decode image code

--- src/main/java/com/ic/er/Render.java
+++ src/main/java/com/ic/er/Render.java
@@ -6,10 +6,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
 
 
 public class Render {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws
+            IOException, InterruptedException {
         try (Playwright playwright = Playwright.create()) {
             List<BrowserType> browserTypes = Arrays.asList(
                     playwright.chromium()
@@ -21,10 +27,25 @@ public class Render {
                     String filePath = new File("src/main/resources/Render.html").toURI().toURL().toString();
                     page.navigate(filePath);
                     System.out.println("image: " + page.locator("#image").textContent());
+                    decoder(page.locator("#image").textContent(),"test2.jpeg");
                 }
             }
         }
-        // load page using HTML Unit and fire scripts
+    }
+
+    public static void decoder(String base64Image, String pathFile) {
+        try (FileOutputStream imageOutFile = new FileOutputStream(pathFile)) {
+            // Converting a Base64 String into Image byte array
+            byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
+            imageOutFile.write(imageByteArray);
+        } catch (FileNotFoundException e) {
+            System.out.println("Image not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading the Image " + ioe);
+        }
+    }
+}
+// load page using HTML Unit and fire scripts
 //        WebClient webClient = new WebClient(BrowserVersion.CHROME);
 //        webClient.getOptions().setJavaScriptEnabled(true);
 //        HtmlPage myPage = webClient.getPage(new File("src/main/resources/Render.html").toURI().toURL());
@@ -40,9 +61,5 @@ public class Render {
 //        }
 //        System.out.println("image: " + myPage.getElementById("image").asNormalizedText());
 //        webClient.close();
-    }
-
-}
-
 
 
